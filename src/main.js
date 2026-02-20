4ca5767e187918f5128486796cd34913ec188795
Kate Sakharova <kate.sakharova27@gmail.com>
2026-02-20 15:33:27 +0300

    Перевела таблицу на загрузку данных с сервера

--- src/main.js
+++ src/main.js
@@ -12,26 +12,26 @@ import { initSorting } from "./components/sorting.js";
 import { initFiltering } from "./components/filtering.js";
 import { initSearching } from "./components/searching.js";
 
-const { data, ...indexes } = initData(sourceData);
+const api = initData(sourceData);
 
 function collectState(form) {
-  const state = processFormData(new FormData(form));
+    const state = processFormData(new FormData(form));
 
-  const rowsPerPage = parseInt(state.rowsPerPage);
-  const page = parseInt(state.page ?? 1);
+    const rowsPerPage = parseInt(state.rowsPerPage);
+    const page = parseInt(state.page ?? 1);
 
-  return {
-    ...state,
-    rowsPerPage,
-    page
-  };
+    return {
+        ...state,
+        rowsPerPage,
+        page
+    };
 }
 
 const sampleTable = initTable({
-  tableTemplate: 'table',
-  rowTemplate: 'row',
-  before: ['search', 'header', 'filter'],
-  after: ['pagination']
+    tableTemplate: 'table',
+    rowTemplate: 'row',
+    before: ['search', 'header', 'filter'],
+    after: ['pagination']
 }, render);
 
 const applySearching = initSearching('search');
@@ -41,33 +41,40 @@ const applySearching = initSearching('search');
 // });
 
 const applySorting = initSorting([
-  sampleTable.header.elements.sortByDate,
-  sampleTable.header.elements.sortByTotal
+    sampleTable.header.elements.sortByDate,
+    sampleTable.header.elements.sortByTotal
 ]);
 
 const applyPagination = initPagination(
-  sampleTable.pagination.elements,
-  (el, page, isCurrent) => {
-    const input = el.querySelector('input');
-    const label = el.querySelector('span');
-    input.value = page;
-    input.checked = isCurrent;
-    label.textContent = page;
-    return el;
-  }
+    sampleTable.pagination.elements,
+    (el, page, isCurrent) => {
+        const input = el.querySelector('input');
+        const label = el.querySelector('span');
+        input.value = page;
+        input.checked = isCurrent;
+        label.textContent = page;
+        return el;
+    }
 );
 
-function render(action) {
-  const state = collectState(sampleTable.container);
+async function render(action) {
+    const state = collectState(sampleTable.container);
 
-  let result = data;
+    let query = {};
 
-  // result = applySearching(result, state, action);
-  // result = applyFiltering(result, state, action);
-  // result = applySorting(result, state, action);
-  // result = applyPagination(result, state, action);
+    // result = applySearching(result, state, action);
+    // result = applyFiltering(result, state, action);
+    // result = applySorting(result, state, action);
+    // result = applyPagination(result, state, action);
 
-  sampleTable.render(result);
+    const { total, items } = await api.getRecords(query);
+    
+    sampleTable.render(items);
 }
 
-render();
+async function init() {
+    const indexes = await api.getIndexes();
+    // здесь потом будем использовать indexes для фильтров
+}
+
+init().then(render);
