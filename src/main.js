0b7d2f969a9b79f1a9ba0bebf090be4c6e3e38f8
Kate Sakharova <kate.sakharova27@gmail.com>
2026-02-20 14:15:02 +0300

    Убрала обработку данных

--- src/main.js
+++ src/main.js
@@ -36,9 +36,9 @@ const sampleTable = initTable({
 
 const applySearching = initSearching('search');
 
-const applyFiltering = initFiltering(sampleTable.filter.elements, {
-  searchBySeller: indexes.sellers
-});
+// const applyFiltering = initFiltering(sampleTable.filter.elements, {
+//   searchBySeller: indexes.sellers
+// });
 
 const applySorting = initSorting([
   sampleTable.header.elements.sortByDate,
@@ -62,10 +62,10 @@ function render(action) {
 
   let result = data;
 
-  result = applySearching(result, state, action);
-  result = applyFiltering(result, state, action);
-  result = applySorting(result, state, action);
-  result = applyPagination(result, state, action);
+  // result = applySearching(result, state, action);
+  // result = applyFiltering(result, state, action);
+  // result = applySorting(result, state, action);
+  // result = applyPagination(result, state, action);
 
   sampleTable.render(result);
 }
